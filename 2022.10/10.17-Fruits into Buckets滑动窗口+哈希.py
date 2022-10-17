50d6143763b74638461a97eb4e6fbe9aea178485
Marloweeee <ConvFactor@bupt.edu.cn>
2022-10-17 08:47:14 +0800

    10.17 ==O(n)复杂度==

---
+++ 2022.10/10.17-Fruits into Buckets滑动窗口+哈希.py
@@ -0,0 +1,21 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        cnt=Counter()
+
+        left=ans=0
+        for right,x in enumerate(fruits):
+            cnt[x]+=1
+            while len(cnt)>2:
+                cnt[fruits[left]]-=1
+                if cnt[fruits[left]]==0:
+                    cnt.pop(fruits[left])
+                left+=1
+
+            ans=max(right-left+1,ans)
+
+
+        return ans
+
